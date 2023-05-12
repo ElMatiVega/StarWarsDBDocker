e44f1b39ba403e22bbbc6e7f17c18f21c2b44ccb
Matías Vega <elmativega3@gmail.com>
2023-05-11 22:38:23 -0300

    crecion de gateway y se le dio la funcionalidad de escuchar a los micros servicios

--- gateWay/index.js
+++ gateWay/index.js
@@ -7,8 +7,20 @@ app.use(morgan("dev"))
 app.use("/characters", createProxyMiddleware({
 	target:"http://localhost:8001",
 	changeOrigin:true
-})
-)
+}))
+
+app.use("/films", createProxyMiddleware({
+	target:"http://localhost:8002",
+	changeOrigin:true
+}))
+
+
+app.use("/planets", createProxyMiddleware({
+	target:"http://localhost:8003",
+	changeOrigin:true
+}))
+
+
 app.listen(8000, ()=>{
     console.log('Gateway on port 8000');
 })
