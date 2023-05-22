3d5fb7e6a35924e2927d74a36d0cf92e9650ad1f
Matías Vega <elmativega3@gmail.com>
2023-05-22 18:21:49 -0300

    laksdhf

--- films/src/data/index.js
+++ films/src/data/index.js
@@ -1,5 +1,5 @@
 const films=require('./films.json')
-
+const axios = require("axios");
 module.exports={
     list: async ()=>{
         const results = await axios.get("http://database:8004/Film");
