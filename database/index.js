cbe0869fcd7fcb0ce3b3a3e430bbba3568435f56
Matías Vega <elmativega3@gmail.com>
2023-05-23 18:43:27 -0300

    addeteo database

---
+++ database/index.js
@@ -0,0 +1,21 @@
+const server = require("./src/server");
+//const {Character, Film, Planet}=require('./src/database');
+
+// Character.list().then((res) =>console.log(res));
+
+// Character.get(1).then((res)=>console.log(res));
+
+// Character.insert({
+//   _id:"202",
+//   name:"Matt",
+//   birth_year:"1982",
+//   gender: 1
+ 
+// }).then((res)=>console.log(res))
+
+// Film.list().then((res) =>console.log(res));
+// Planet.get(1).then((res)=>console.log(res));
+
+server.listen(8004, () => {
+  console.log(`DataBase Server listening on port 8004`);
+});
---
+++ database/src/database/schemas/characterSchema.js
@@ -0,0 +1,36 @@
+const { Schema } = require('mongoose');
+
+const characterSchema = new Schema({
+    _id: String ,// Schema.Types.ObjectId es la configu por defecto que le da mongo. Pero nosotros ya le pusimos un string"1"
+    name: String ,
+    height: String ,
+    mass: String ,
+    hair_color: String ,
+    skin_color: String ,
+    eye_color: String ,
+    birth_year: String ,
+    gender: String ,
+    homeworld:{type:String, ref:"Planet"} ,// "1" prestar atencion: hace ref al id del plantea
+    films: [{type:String, ref:"Film"}],// ["1", "2", "3", "6"]referencia a peliculas
+});
+
+//
+//no arrow fn
+characterSchema.statics.list= async function(){
+ return await this.find()
+ .populate('homeworld', ['_id','name'])
+ .populate('films',['_id','title'])
+}
+
+characterSchema.statics.get= async function(id){
+    return await this.findById(id)
+                    .populate('homeworld', ['_id','name'])
+                    .populate('films',['_id','title'])
+
+}
+
+characterSchema.statics.insert= async function(character){
+    return await this.create(character)
+}
+
+module.exports=characterSchema;
